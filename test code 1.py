656d3cd8cabb9484adeaa4acdbb1b1d7c3d5a340
adriancoward <adrian.v.coward@gmail.com>
2022-05-28 13:21:15 +0100

    Created test class

--- test code 1.py
+++ test code 1.py
@@ -2,3 +2,13 @@ print("Test message from another Python code in a new repository")
 print("and another message")
 print("this line was added after creating Test Branch 1")
 print("this line added after merging back Test Branch 1")
+
+class test_class:
+    def __init__(self,prop1,prop2):
+        self.prop1=prop1
+        self.prop2=prop2
+
+#create an instance of the test_class
+test_class1=test_class(1,2)
+
+print(test_class1.prop2)
